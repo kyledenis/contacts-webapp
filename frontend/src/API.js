aa27e5616b7d3f91c8bb83801a10ab6e0f15aeaf
Kyle Denis <kylenden@gmail.com>
2023-10-19 01:39:04 +1100

    Created phones API

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -44,3 +44,38 @@ export const deleteContact = async (id) => {
         throw error;
     }
 };
+
+export const getPhones = async (contactId) => {
+    try {
+        return await getData(`${API_ENDPOINT}/${contactId}/phones`);
+    } catch (error) {
+        console.log("Error getting phones:", error);
+        throw error;
+    }
+};
+
+export const createPhone = async (contactId, phone) => {
+    try {
+        return await getData(`${API_ENDPOINT}/${contactId}/phones`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...phone, contactId }),
+        });
+    } catch (error) {
+        console.log("Error creating phone:", error);
+        throw error;
+    }
+};
+
+export const deletePhone = async (contactId, phoneId) => {
+    try {
+        return await getData(`${API_ENDPOINT}/${contactId}/phones/${phoneId}`, {
+            method: "DELETE",
+        });
+    } catch (error) {
+        console.log("Error deleting phone:", error);
+        throw error;
+    }
+};
