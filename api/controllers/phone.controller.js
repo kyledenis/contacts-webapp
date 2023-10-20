39436f580d8928c0bacbde36671a4221027e8d29
Kyle Denis <kylenden@gmail.com>
2023-10-21 00:34:10 +1100

    Refactored code for readability. Added logging. Improved error handling.

--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -2,8 +2,36 @@ const db = require("../models");
 const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
+const logRequestStart = (message) => {
+    console.log("---------- NEW REQUEST ----------");
+    console.log(message);
+};
+
+const logError = (message, err) => {
+    console.log(message, err);
+};
+
+const sendError = (res, message, status = 500) => {
+    res.status(status).send({
+        message,
+    });
+};
+
+const validatePhoneRequest = (req, res) => {
+    if (!req.body.name || !req.body.number || !req.body.contactId) {
+        logError("Missing fields, aborting operation");
+        sendError(res, "Name, Number and ContactID cannot be empty", 400);
+        return false;
+    }
+    return true;
+};
+
 // Create phone
 exports.create = (req, res) => {
+    logRequestStart("Received POST request for phone creation");
+
+    if (!validatePhoneRequest(req, res)) return;
+
     const phone = {
         name: req.body.name,
         number: req.body.number,
@@ -15,26 +43,30 @@ exports.create = (req, res) => {
             res.send(data);
         })
         .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
+            logError("Error encountered:", err);
+            sendError(
+                res,
+                err.message ||
                     "An error occurred while creating the phone number",
-            });
+            );
         });
 };
 
 // Get all phones
 exports.findAll = (req, res) => {
+    logRequestStart("Received GET request to fetch all phone numbers");
+
     Phones.findAll({ where: { contactId: req.params.contactId } })
         .then((data) => {
             res.send(data);
         })
         .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
+            logError("Error encountered:", err);
+            sendError(
+                res,
+                err.message ||
                     "An error occurred while retrieving phone numbers",
-            });
+            );
         });
 };
 
@@ -42,22 +74,27 @@ exports.findAll = (req, res) => {
 exports.findOne = (req, res) => {
     const phoneId = req.params.phoneId;
 
+    logRequestStart(`Received GET request to fetch phone with id: ${phoneId}`);
+
     Phones.findByPk(phoneId)
         .then((data) => {
             if (!data) {
-                res.status(404).send({
-                    message: `Phone number with id ${phoneId} not found`,
-                });
+                sendError(
+                    res,
+                    `Phone number with id ${phoneId} not found`,
+                    404,
+                );
             } else {
                 res.send(data);
             }
         })
         .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
-                    `An error occurred while retrieving phone number with id ${phoneId}`,
-            });
+            logError("Error encountered:", err);
+            sendError(
+                res,
+                err.message ||
+                    `Error retrieving phone number with id ${phoneId}`,
+            );
         });
 };
 
@@ -65,24 +102,28 @@ exports.findOne = (req, res) => {
 exports.update = (req, res) => {
     const phoneId = req.params.phoneId;
 
+    logRequestStart(`Received PUT request to update phone (id: ${phoneId})`);
+
     Phones.update(req.body, { where: { id: phoneId } })
         .then((num) => {
-            if (num == 1) {
+            if (num === 1) {
                 res.send({
                     message: `Phone number with id ${phoneId} was updated successfully`,
                 });
             } else {
-                res.send({
-                    message: `Phone number with id ${phoneId} was not found`,
-                });
+                sendError(
+                    res,
+                    `Cannot update phone number with id ${phoneId}. Phone not found`,
+                    404,
+                );
             }
         })
         .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
-                    `An error occurred while updating phone number with id ${phoneId}`,
-            });
+            logError("Error encountered:", err);
+            sendError(
+                res,
+                err.message || `Error updating phone number with id ${phoneId}`,
+            );
         });
 };
 
@@ -90,23 +131,29 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
     const phoneId = req.params.phoneId;
 
+    logRequestStart(
+        `Received DELETE request for phone deletion (id: ${phoneId})`,
+    );
+
     Phones.destroy({ where: { id: phoneId } })
         .then((num) => {
-            if (num == 1) {
+            if (num === 1) {
                 res.send({
                     message: `Phone number with id ${phoneId} was deleted successfully`,
                 });
             } else {
-                res.send({
-                    message: `Phone number with id ${phoneId} was not found`,
-                });
+                sendError(
+                    res,
+                    `Cannot delete phone number with id ${phoneId}. Phone not found`,
+                    404,
+                );
             }
         })
         .catch((err) => {
-            res.status(500).send({
-                message:
-                    err.message ||
-                    `An error occurred while deleting phone number with id ${phoneId}`,
-            });
+            logError("Error encountered:", err);
+            sendError(
+                res,
+                err.message || `Error deleting phone number with id ${phoneId}`,
+            );
         });
 };
